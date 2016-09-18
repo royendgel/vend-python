f01a3f34cfe4c0c6efd661021c1a126ac9687153
Royendgel Silberie <royendgel@gmail.com>
2016-09-18 12:29:44 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='vend',
+      version='0.1',
+      description='vend pos python',
+      url='https://github.com/royendgel/vend-python',
+      author='Royendgel Silberie',
+      author_email='rsilberie@techprocur.com',
+      license='MIT',
+      packages=['vend'],
+      zip_safe=False)
